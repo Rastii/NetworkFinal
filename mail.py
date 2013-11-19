86413455d543bb2271feaaf1b3246c1a8eabfde7
Andrew Calvano <acalvano@Erebus.(none)>
2013-11-19 13:53:01 -0500

    Added mail.py file

---
+++ mail.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+#Computer Networks .2.mail Project Implementation
+# By Lukyan Hritsko and Andrew Calvano
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
