9ee4d8b4a55b872e714d2ab387d4e807b4215f0b
Rastii <rastii09@gmail.com>
2013-12-01 20:56:26 -0500

    Test script V2

--- dotTestScript.py
+++ dotTestScript.py
@@ -1,6 +1,8 @@
 import subprocess
 import sys
 import hashlib
+from os import system
+from time import sleep
 
 def get_file_names(directory):
   proc = subprocess.Popen(["ls", directory], stdout=subprocess.PIPE)
@@ -18,12 +20,24 @@ def main():
     exit(1)
 
   expected_md5 = md5_hash_file(sys.argv[1])
+  file_name = sys.argv[1]
+  print "RUNNING TEST ON %s" % file_name
+  print "USING DOTSENDER"
+  for x in xrange(25):
+    proc = subprocess.Popen(['python', 'dotSender.py', '127.0.0.1', file_name])
+    proc.wait()
+    #system('python dotSender.py 127.0.0.1 ' + file_name)
+  system('mv recvEmail* recv_tests/')
+  print "END DOTSENDER"
+  sleep(1)
 
+  print "BEGIN CHECKING MD5SUMS"
   for f in get_file_names(sys.argv[2]):
     print "Testing file %s" % f
     if md5_hash_file(sys.argv[2] + "/" + f) != expected_md5:
       print "File %s does not match." % f
-      exit(1)
+      system("diff -u %s %s/%s" % (sys.argv[1], sys.argv[2], f))      
+  print "END MD5SUM CHECKING"
 
 if __name__ == "__main__":
   main()
